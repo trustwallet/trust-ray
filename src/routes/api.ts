63f2f809aeeed3af53c6ca33bdb73b50940e41cd
Mykola Radchenko <kolya182@gmail.com>
2017-11-04 14:30:31 -0700

    Add statusController

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,16 +1,18 @@
 import  * as express from "express";
 import { TransactionController } from "../controllers/transaction.controller";
 import { TokenController } from "../controllers/token.controller";
+import { StatusController } from "../controllers/status.controller";
 
 const router = express["Router"]();
 
 // load controllers
 const transactionController = new TransactionController();
 const tokenController = new TokenController();
-
+const statusController = new StatusController();
 // define the routes
 
 // URLs for transactions
+router.get("/", statusController.getStatus);
 router.get("/transactions", transactionController.readAllTransactions);
 router.get("/transactions/:transactionId", transactionController.readOneTransaction);
 
