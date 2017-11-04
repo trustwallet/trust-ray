90e06560ac03cea32b90be264533f54142483260
Philipp Rieger <phoebus91@web.de>
2017-11-04 14:13:57 +0100

    Added ERC20 Transaction as a subdocument to the transaction model. Thereby we do not need a separate model reducing complexity.

--- src/models/transaction.model.ts
+++ src/models/transaction.model.ts
@@ -2,6 +2,42 @@ const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate");
 const Schema = mongoose.Schema;
 
+
+/**
+ * Sub document of a transaction -
+ * filled when the transaction destination
+ * is a ERC20 token contract.
+ *
+ * @type {"mongoose".Schema}
+ */
+const tokenTransactionSchema = new Schema({
+    transactionType: {
+        type: String,
+        required: true
+    },
+    contract: {
+        type: String,
+        required: true
+    },
+    from: {
+        type: String,
+        required: true
+    },
+    to: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: String,
+        required: true
+    }
+});
+
+/**
+ * Model for a single transaction.
+ *
+ * @type {"mongoose".Schema}
+ */
 const transactionSchema = new Schema({
     _id: {
         type: String,
@@ -48,7 +84,9 @@ const transactionSchema = new Schema({
     gasUsed: {
         type: String,
         required: true
-    }
+    },
+    action: tokenTransactionSchema
+
 }, { versionKey: false,
      _id: false
 });
