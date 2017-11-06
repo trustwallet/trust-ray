690c927ace63ccb22d885a79f625948681aa0d9a
Michael Scoff <thetrustwallet@gmail.com>
2017-11-06 02:19:11 -0800

    Show latest block number in the chain

--- src/controllers/status.controller.ts
+++ src/controllers/status.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { sendJSONresponse } from "../common/utils";
 import { Transaction } from "../models/transaction.model";
 import { LastParsedBlock } from "../models/lastParsedBlock.model";
+import { Config } from "../common/config";
 
 import * as winston from "winston";
 const packageJSON = require("../../package.json");
@@ -11,11 +12,13 @@ export class StatusController {
     getStatus(req: Request, res: Response) {
         Promise.all([
             Transaction.count(),
-            LastParsedBlock.findOne()
-        ]).then(([transactionsCount, lastParsedBlock]) => {
+            LastParsedBlock.findOne(),
+            Config.web3.eth.getBlockNumber(),
+        ]).then(([transactionsCount, lastParsedBlock, latestBlockNumberInBC]) => {
             sendJSONresponse(res, 200, {
                 transactions: parseInt(transactionsCount).toLocaleString(),
-                lastParsedBlock: lastParsedBlock.lastBlock,
+                latestBlockNumberInBC,
+                latestBlockNumberInDB: lastParsedBlock.lastBlock,
                 version: packageJSON.version,
             });
         }).catch((err: Error) => {
