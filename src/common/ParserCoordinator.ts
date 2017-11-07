fc23dbb8ad39b3cbc4fc9bc7f6c6803e5b1acbfa
Philipp Rieger <phoebus91@web.de>
2017-11-07 12:22:35 +0100

    Added comments of idea of events to ParseCoordinator.

--- src/common/ParserCoordinator.ts
+++ src/common/ParserCoordinator.ts
@@ -1,9 +1,27 @@
 
 class TransactionParser {
-    public parseTransactionAction(transaction: any) {}
+
+    public parseTransactionAction(transaction: any) {
+        // parse the transaction type/action
+        // put it into subdocument "action"
+        // save it into DB
+        // and trigger event via:
+        // process.emit("transactionTypeParsed")
+    }
+
+    public start() {
+        // parse transactions and trigger event for every
+        // parsed transaction via:
+        // process.emit("baseTransactionParsed", transaction)
+    }
 }
 class TokenParser {
-    public parseTokenBalance(transaction: any) {}
+
+    public parseTokenBalance(transaction: any) {
+        // parse the token balance changes from the
+        // transaction action and update the
+        // balances in the DB
+    }
 }
 
 export class ParserCoordinator {
@@ -19,11 +37,12 @@ export class ParserCoordinator {
         });
 
         process.on("transactionTypeParsed", (transaction: any) => {
-            // save transaction
-
-            // start token parsing
             this.tokenParser.parseTokenBalance(transaction);
         });
+    }
+
+    public start() {
+        this.transactionParser.start();
     }
 
 }
