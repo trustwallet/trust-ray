08a3b25bb40b5a5870ac3106569b489551c9964d
Michael Scoff <thetrustwallet@gmail.com>
2017-11-08 01:55:36 -0800

    Update ChainParser

--- src/common/chainParser.ts
+++ src/common/chainParser.ts
@@ -10,17 +10,23 @@ const erc20abi = require("./erc20abi");
 
 export class ChainParser {
 
-    private contractsCache: any = {};
-
     public start() {
         winston.info("start chain parsing...");
         this.getBlockState().then(([blockInChain, blockInDB]) => {
-            const concurentBlocks = 40
-            winston.info("blockInChain: " + blockInChain + " blockInDB: " + blockInDB);
+            let startBlock: number;
             if (!blockInDB) {
-                this.startBlock(0, blockInChain, concurentBlocks);
+                startBlock = 1
             } else {
-                this.startBlock(blockInDB.lastBlock, blockInChain, concurentBlocks);
+                startBlock = blockInDB.lastBlock
+            }
+            const concurentBlocks = 40
+            
+            winston.info("blockInDB: " + startBlock + ", blockInChain: " + blockInChain);
+
+            if (startBlock < blockInChain) {
+                this.startBlock(startBlock, blockInChain, concurentBlocks);
+            } else {
+                this.scheduleToRestart(10000);
             }
         }).catch((err: Error) => {
             winston.error("Failed to load initial block state: " + err);
@@ -40,26 +46,29 @@ export class ChainParser {
         const range = (start: number, end: number) => (
             Array.from(Array(end - start + 1).keys()).map(i => i + start)
         );
-        const endBlock = startBlock + Math.max(concurentBlocks - 1, 1);
+        const endBlock = startBlock + Math.min(concurentBlocks, lastBlock - startBlock);
         const numberBlocks = range(startBlock, endBlock);
+        
         const promises = numberBlocks.map((number) => { return this.parseBlock(number)});
         Promise.all(promises).then((blocks: any[]) => {
             return this.saveTransactions(blocks);
         }).then((results: any) => {
             this.saveLastParsedBlock(endBlock);
-            if (startBlock < lastBlock) {
-                this.startBlock(endBlock, lastBlock, concurentBlocks);
+            if (endBlock < lastBlock) {
+                this.startBlock(endBlock + 1, lastBlock, concurentBlocks);
             } else {
                 winston.info("Last block is parsed on the blockchain, waiting for new blocks");
-                this.delay(5000).then(() => {
-                    this.start()
-                })
+                this.scheduleToRestart(10000)
             }
         }).catch((err: Error) => {
             winston.error("failed to parse: " + err + ". restart again startBlock: " + startBlock + ", lastBlock: " + lastBlock);
-            this.delay(1000).then(() => {
-                this.startBlock(startBlock, lastBlock, concurentBlocks);
-            })
+            this.startBlock(startBlock, lastBlock, concurentBlocks);
+        })
+    }
+
+    private scheduleToRestart(delay: number) {
+        this.delay(delay).then(() => {
+            this.start()
         })
     }
 
@@ -110,7 +119,52 @@ export class ChainParser {
         if (bulkTransactions.length === 0) {
             return Promise.resolve()
         }
-        return bulkTransactions.execute()
+        return bulkTransactions.execute();
+    }
+
+    private findOrCreateERC20Contract(contract: String): Promise<void> {
+        return ERC20Contract.findOne({_id: contract}).exec().then((erc20contract: any) => {
+            if (!erc20contract) {
+                return this.getContract(contract)
+            } else {
+                return Promise.resolve(erc20contract)
+            }
+        })
+    }
+
+    private getContract(contract: String): Promise<void> {
+        const contractInstance = new Config.web3.eth.Contract(erc20abi, contract);
+        
+        const p1 = contractInstance.methods.name().call().catch((err: Error) => {
+            winston.error(`Could not get name of contract ${contract} with error: ${err}`);
+        });
+        const p2 = contractInstance.methods.totalSupply().call().catch((err: Error) => {
+            winston.error(`Could not get total supply of contract ${contract} with error: ${err}`);
+        });
+        const p3 = contractInstance.methods.decimals().call().catch((err: Error) => {
+            winston.error(`Could not get decimals of contract ${contract} with error: ${err}`);
+        });
+        const p4 = contractInstance.methods.symbol().call().catch((err: Error) => {
+            winston.error(`Could not get symbol of contract ${contract} with error: ${err}`);
+        });
+        return Promise.all([p1, p2, p3, p4]).then(([name, totalSupply, decimals, symbol]: any[]) => {
+            return this.updateERC20Token(contract, {name, totalSupply, decimals, symbol});
+        }).catch((err: Error) => {
+            winston.error(`Could not wait to get all information for contract ${contract} while processing its input with error: ${err}`);
+        });
+    }
+
+    private updateERC20Token(contract: String, obj: any): Promise<void> {
+        return ERC20Contract.findOneAndUpdate({_id: contract}, 
+            {
+                _id: contract,
+                 name: obj.name, 
+                 totalSupply: obj.totalSupply, 
+                 decimals: obj.decimals,
+                 symbol: obj.symbol
+            }, {upsert: true, returnNewDocument: true}).then((res: any) => {
+            return ERC20Contract.findOne({_id: contract}).exec();
+        })
     }
 
     private processTransactionType(transaction: any) {
@@ -123,70 +177,11 @@ export class ChainParser {
             const contract = transaction.to.toLowerCase();
             const from = transaction.from.toLowerCase();
 
-            if (this.contractsCache.hasOwnProperty(contract)) {
-
-                // contract is cached, no further work needed
-
-                return new Promise((resolve) => {
-                    resolve({
-                        transactionType: "token_transfer",
-                        contract: contract,
-                        to: to,
-                        from: from,
-                        value: value,
-                        name: this.contractsCache[contract].name,
-                        totalSupply: this.contractsCache[contract].totalSupply,
-                        decimals: this.contractsCache[contract].decimals,
-                        symbol: this.contractsCache[contract].symbol
-                    });
-                });
-
-            } else {
-
-                // contract is not cached, initiate contract
-                // in order to get required attributes
-
-                const contractInstance = new Config.web3.eth.Contract(erc20abi, contract);
-
-                const p1 = contractInstance.methods.name().call().catch((err: Error) => {
-                    winston.error(`Could not get name of contract ${contract} with error: ${err}`);
-                });
-                const p2 = contractInstance.methods.totalSupply().call().catch((err: Error) => {
-                    winston.error(`Could not get total supply of contract ${contract} with error: ${err}`);
-                });
-                const p3 = contractInstance.methods.decimals().call().catch((err: Error) => {
-                    winston.error(`Could not get decimals of contract ${contract} with error: ${err}`);
-                });
-                const p4 = contractInstance.methods.symbol().call().catch((err: Error) => {
-                    winston.error(`Could not get symbol of contract ${contract} with error: ${err}`);
-                });
-
-                return Promise.all([p1, p2, p3, p4]).then((values: any) => {
-
-                    // cache values for next access
-                    this.contractsCache[contract] = {
-                        name: values[0],
-                        totalSupply: values[1],
-                        decimals: values[2],
-                        symbol: values[3]
-                    };
-
-                    return {
-                        transactionType: "token_transfer",
-                        contract: contract,
-                        to: to,
-                        from: from,
-                        value: value,
-                        name: values[0],
-                        totalSupply: values[1],
-                        decimals: values[2],
-                        symbol: values[3]
-                    };
-
-                }).catch((err: Error) => {
-                    winston.error(`Could not wait to get all information for contract ${contract} while processing its input with error: ${err}`);
-                });
-            }
+            this.findOrCreateERC20Contract(contract).then((erc20contract: any) => {
+                winston.info("contract found: ", erc20contract)
+            }).catch((err: Error) => {
+                winston.error(`Could find or create contract ${contract}, error: ${err}`);
+            });
         }
     }
 
