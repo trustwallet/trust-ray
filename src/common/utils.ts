e35e687feb0255134cc8413c5008ad939004d533
Philipp Rieger <phoebus91@web.de>
2017-11-11 15:26:11 +0100

    Added removeScientificNotationFromNumbString() function.

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -10,3 +10,35 @@ export function sendJSONresponse(res: Response, status: number, content: any) {
     res.status(status);
     res.json(content);
 }
+
+/**
+ * Converts a number given as string containing scientific
+ * notation to regular number, e.g.:
+ * 1.105898485e+22 to 11058984850000000000000
+ *
+ * @param {string} numberString
+ * @returns {string}
+ */
+export function removeScientificNotationFromNumbString(numberString: string) {
+    const numb = +numberString;
+    const data = String(numb).split(/[eE]/);
+
+    if (data.length == 1)
+        return data[0];
+
+    let z = "";
+    const sign = numb < 0 ? "-" : "";
+    const str = data[0].replace(".", "");
+    let mag = Number(data[1]) + 1;
+
+    if (mag < 0) {
+        z = sign + "0.";
+        while (mag++)
+            z += "0";
+        return z + str.replace(/^\-/, "");
+    }
+    mag -= str.length;
+    while (mag--)
+        z += "0";
+    return str + z;
+}
