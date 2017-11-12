f5f10cff674cfa648db3aa155081c0faa519816c
Philipp Rieger <phoebus91@web.de>
2017-11-12 13:23:13 +0100

    Add utility script for pinging Heroku to avoid sleeping mode.

---
+++ scripts/HerokuPinger.py
@@ -0,0 +1,16 @@
+# Purpose: Sending steadily requests to Heroku so that it doesn't switch into sleeping mode
+
+import requests
+import threading
+
+URL = "https://test-trust-wallet-backend.herokuapp.com/"
+
+def main():
+	threading.Timer(60.0 * 8, main).start()	# call main every 8 min
+	print("Sending GET to {}".format(URL))
+	r = requests.get(URL)
+	print("Response: {}".format(r.text.encode('utf-8')))
+
+
+if __name__ == "__main__":
+	main()
