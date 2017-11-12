7a1a095222ca6fed0df98a0e5aa221e9bf955aea
Philipp Rieger <phoebus91@web.de>
2017-11-12 13:25:16 +0100

    Added docu header to heroku pinger script.

--- scripts/HerokuPinger.py
+++ scripts/HerokuPinger.py
@@ -1,9 +1,17 @@
-# Purpose: Sending steadily requests to Heroku so that it doesn't switch into sleeping mode
+###################################################################################################
+#
+# @purpose: Sending steadily requests to Heroku so that it doesn't switch into sleeping mode
+# @author: Philipp Rieger
+#
+###################################################################################################
+
 
 import requests
 import threading
 
+
 URL = "https://test-trust-wallet-backend.herokuapp.com/"
+
 
 def main():
 	threading.Timer(60.0 * 8, main).start()	# call main every 8 min
