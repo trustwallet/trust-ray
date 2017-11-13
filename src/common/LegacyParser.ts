6fa46d2f117656489efb1907cbc329d8caf55395
Philipp Rieger <phoebus91@web.de>
2017-11-13 22:21:00 +0100

    Added LegacyParser for old models.

---
+++ src/common/LegacyParser.ts
@@ -0,0 +1,39 @@
+import { Transaction } from "../models/TransactionModel";
+import { TokenParser } from "./TokenParser";
+import { TransactionParser } from "./TransactionParser";
+
+
+export class LegacyParser {
+
+    public reparseChain() {
+        Transaction.find({
+            $or: [
+                {operations: { $exists: false }},
+                {operations: { $eq: [] }},
+            ],
+        }).limit(100).exec().then((transactions: any) => {
+            if (transactions) {
+                return new TokenParser().parseERC20Contracts(transactions).then(([transactions, contracts]: any) => {
+                    new TransactionParser().parseTransactionOperations(transactions, contracts);
+                });
+            } else {
+                Promise.resolve();
+            }
+        }).then(() => {
+            this.scheduleToRestart(1000);
+        });
+    }
+
+    private scheduleToRestart(delay: number) {
+        this.delay(delay).then(() => {
+            this.reparseChain();
+        });
+    }
+
+    private delay(t: number) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, t);
+        });
+    }
+
+}
