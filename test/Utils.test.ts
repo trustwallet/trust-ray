50f8c4c05e365a98e42cdea61512de10f8e6e976
Mykola Radchenko <kolya182@gmail.com>
2017-11-14 15:36:08 -0800

    Change test description

--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -15,7 +15,7 @@ describe('Test Util.ts', () => {
                 expect(result).to.be.equal('22093167076133973000000');
                 expect(result).to.be.a('string');
             });
-            it('Converts a number given as string containing scientific notation #3', () => {
+            it('Returns number given as string', () => {
                 const result = removeScientificNotationFromNumbString('1265');
                 expect(result).to.be.equal('1265');
                 expect(result).to.be.a('string');
