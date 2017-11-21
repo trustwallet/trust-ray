c66f0b1dc876f137b924036d86eb64a3b1262934
Michael Scoff <thetrustwallet@gmail.com>
2017-11-21 09:35:35 -0800

    Operation localized (#62)
    * Add operations_localozed property to /:transcations return object
    
    * Reuse variable
    
    * Add symbol property and remove action
    
    * Expose LocalizedOperationConverter as a class
    
    * Lowecase name

---
+++ src/common/operations/LocalizedOperationConverter.ts
@@ -0,0 +1,25 @@
+export class LocalizedOperationConverter {
+    public static from(operations: Array<Object>) {
+        return operations.map((operation: any) => {
+            const type = operation.type;
+            switch (type) {
+                case "token_transfer": return this.tokenTransfer(operation);
+            }
+        });
+    };
+
+    private static tokenTransfer(operation: any) {
+        const contract = operation.contract;
+        const value = operation.value / 10 ** contract.decimals;
+        return {
+            "title": `Transfer ${contract.symbol}`,
+            "type": operation.type,
+            "from": operation.from,
+            "to": operation.to,
+            "contract": contract.address,
+            "value": String(value),
+            "symbol": contract.symbol,
+        }
+    };
+}
+
