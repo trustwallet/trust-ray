2d432cb6db68e61b9214386b4c342f1cf9035f1f
Mykola Radchenko <kolya182@gmail.com>
2017-12-11 18:30:52 -0800

    Add readPendingTransactions functionality

--- src/controllers/PendingTransactionController.ts
+++ src/controllers/PendingTransactionController.ts
@@ -1,1 +1,52 @@
+
+import { Request, Response } from "express";
+import { sendJSONresponse } from "../common/Utils";
+import { Config } from "../common/Config";
+import { Error } from "mongoose";
+let Promise = require("bluebird");
+
+export class PendingTransactionController {
+
+    public readPendingTransaction(req: Request, res: Response) {
+        const validationErrors: any = PendingTransactionController.validateQueryParameters(req);
+
+        if (validationErrors) {
+            sendJSONresponse(res, 400, {validationErrors});
+            return;
+        }
+
+        const address: string = req.query.address;
+
+        Config.web3.eth.getBlock("pending", true)
+        .then((pendings: any) => {
+            return new Promise.filter(pendings.transactions, function(transaction: any) {
+                return (transaction.from === address || transaction.to == address) ? true : false;
+            }).map(function(filteredTransactions: any) {
+                return {
+                    from: filteredTransactions.from,
+                    to: filteredTransactions.to,
+                    blockNumber: filteredTransactions.blockNumber,
+                    gas: filteredTransactions.gas,
                     gasPrice: filteredTransactions.gasPrice,
+                    nonce: filteredTransactions.nonce,
+                    hash: filteredTransactions.hash,
+                    input: filteredTransactions.input,
+                }
+            });
+        })
+        .then((pendings: any) => {
+            sendJSONresponse(res, 200, { pendings });
+            return;
+        })
+        .catch((error: Error) => {
+            sendJSONresponse(res, 400, { error });
+            return; 
+        });
+    }
+
+    private static validateQueryParameters(req: Request) {
+        req.checkQuery("address", "address needs to be alphanumeric and 42 characters long").optional().isLength({min: 42, max: 42});
+        req.checkQuery("address", "address needs to be hexidecimal").optional().isAlphanumeric();
+        return req.validationErrors();
+    }
+};
