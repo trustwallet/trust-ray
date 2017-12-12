2e74e489604d9681dd3d926876f271a384fdcbb2
Mykola Radchenko <kolya182@gmail.com>
2017-12-11 17:47:12 -0800

    Add /pendingTransactions endpoint

--- src/routes/ApiRoutes.ts
+++ src/routes/ApiRoutes.ts
@@ -2,6 +2,7 @@ import  * as express from "express";
 import { TransactionController } from "../controllers/TransactionController";
 import { TokenController } from "../controllers/TokenController";
 import { StatusController } from "../controllers/StatusController";
+import { PendingTransactionController } from "../controllers/PendingTransactionController";
 
 const router = express["Router"]();
 
@@ -9,12 +10,16 @@ const router = express["Router"]();
 const transactionController = new TransactionController();
 const tokenController = new TokenController();
 const statusController = new StatusController();
+const pendingTransactionController = new PendingTransactionController();
 // define the routes
 
 // URLs for transactions
 router.get("/", statusController.getStatus);
 router.get("/transactions", transactionController.readAllTransactions);
 router.get("/transactions/:transactionId", transactionController.readOneTransaction);
+
+//URLs for pending transactions
+router.get("/pendingTransactions", pendingTransactionController.readPendingTransaction);
 
 // URLs for tokens
 router.get("/tokens", tokenController.readAllTokens);
