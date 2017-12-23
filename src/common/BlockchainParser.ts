b5ab1fd41b11b4be1dde07d1bf41ea4f716e5b59
Michael Scoff <thetrustwallet@gmail.com>
2017-12-23 01:11:18 -0800

    Include rebalance block (#84)
    * Include rebalance block
    
    * Remove extra space

--- src/common/BlockchainParser.ts
+++ src/common/BlockchainParser.ts
@@ -18,6 +18,7 @@ export class BlockchainParser {
     private transactionParser: TransactionParser;
     private tokenParser: TokenParser;
     private concurrentBlocks = 1;
+    private rebalanceOffset = 15
 
     constructor() {
         this.transactionParser = new TransactionParser();
@@ -128,6 +129,11 @@ export class BlockchainParser {
         const blocksToProcess = Math.min(this.concurrentBlocks, Math.min(lastBlock - startBlock + 1), 1);
         const sBlock = ascending ? startBlock : Math.max(startBlock - blocksToProcess + 1, 0)
         const numberBlocks = range(sBlock, startBlock + blocksToProcess - 1);
+
+        const rebalanceBlock = startBlock - this.rebalanceOffset
+        if (rebalanceBlock > 0) {
+            numberBlocks.push(rebalanceBlock)
+        }
 
         // parse blocks
         const promises = numberBlocks.map((number) => {
