b4f63f9b3aed48c0ea969b1be5b3432c07683795
Mykola Radchenko <kolya182@gmail.com>
2018-01-09 23:17:51 -0800

    Fix spacing

--- src/pusher/Notification.ts
+++ src/pusher/Notification.ts
@@ -6,80 +6,80 @@ import { Config } from "../common/Config";
 
 export class Notification {
     private push: any;
-	private networkSymbol = process.env.NETWORK_SYMBOL || "ETH";
+    private networkSymbol = process.env.NETWORK_SYMBOL || "ETH";
 
-	private settings = {
-		apn: {
-			token: {
-				key: Buffer.from(process.env.APN_KEY, "base64").toString(),
-				keyId: process.env.APN_KEYID,
-				teamId: process.env.APN_TEAMID,
-			},
-			production: true
-		}
-	}
+    private settings = {
+        apn: {
+            token: {
+                key: Buffer.from(process.env.APN_KEY, "base64").toString(),
+                keyId: process.env.APN_KEYID,
+                teamId: process.env.APN_TEAMID,
+            },
+            production: true
+        }
+    }
 
-	constructor() {
-		 this.push = new PushNotifications(this.settings);
-	}
+    constructor() {
+         this.push = new PushNotifications(this.settings);
+    }
 
-	process(transaction: any, device: any) {
-		winston.info(`Processing device transaction block: ${transaction.blockNumber} id: ${transaction._id}, ${JSON.stringify(device)}`);
+    process(transaction: any, device: any) {
+        winston.info(`Processing device transaction block: ${transaction.blockNumber} id: ${transaction._id}, ${JSON.stringify(device)}`);
 
-		const transactionType = this.getTransactionType(transaction);
-		const addresses: string[] = transaction.addresses;
-		const from: string = addresses[0];
-		const token: string = device.token;
+        const transactionType = this.getTransactionType(transaction);
+        const addresses: string[] = transaction.addresses;
+        const from: string = addresses[0];
+        const token: string = device.token;
 
-		return Promise.mapSeries(device.wallets, (wallet: string) => {
-			const transactionAction = from === wallet ? "Sent" : "Received";
+        return Promise.mapSeries(device.wallets, (wallet: string) => {
+            const transactionAction = from === wallet ? "Sent" : "Received";
 
-			if (addresses.indexOf(wallet) !== -1) {
-				if (transactionType === "transfer") {
-					const title = `${transactionAction} ${this.getValueInEth(transaction.value)} ${this.networkSymbol} from`;
-					const ethMessage = this.createMeassage(title, from);
+            if (addresses.indexOf(wallet) !== -1) {
+                if (transactionType === "transfer") {
+                    const title = `${transactionAction} ${this.getValueInEth(transaction.value)} ${this.networkSymbol} from`;
+                    const ethMessage = this.createMeassage(title, from);
 
-					return this.send(token, ethMessage).then((notificationResult: any) => {
-						winston.info("Notification result :", JSON.stringify(notificationResult));
-					});
-				}
+                    return this.send(token, ethMessage).then((notificationResult: any) => {
+                        winston.info("Notification result :", JSON.stringify(notificationResult));
+                    });
+                }
 
-				if (transactionType === "token_transfer") {
-					const operations = transaction.operations[0];
-					const title = `${transactionAction} ${this.getValueInEth(operations.value)} ${operations.contract.symbol} from`;
-					const tokenMessage = this.createMeassage(title, from);
+                if (transactionType === "token_transfer") {
+                    const operations = transaction.operations[0];
+                    const title = `${transactionAction} ${this.getValueInEth(operations.value)} ${operations.contract.symbol} from`;
+                    const tokenMessage = this.createMeassage(title, from);
 
-					return this.send(token, tokenMessage).then((notificationResult: any) => {
-						winston.info("Notification result :", JSON.stringify(notificationResult));
-					});
-				}
-			}
-		});
-	};
+                    return this.send(token, tokenMessage).then((notificationResult: any) => {
+                        winston.info("Notification result :", JSON.stringify(notificationResult));
+                    });
+                }
+            }
+        });
+    };
 
-	private getValueInEth(value: string): string {
-		return Config.web3.utils.fromWei(value);
-	}
+    private getValueInEth(value: string): string {
+        return Config.web3.utils.fromWei(value);
+    }
 
-	private createMeassage(title: string, from: string): {title: string, body: string, topic: string} {
-		return {
-			title,
-			body: from,
-			topic: process.env.APN_BUNDLEID,
-		}
-	}
+    private createMeassage(title: string, from: string): {title: string, body: string, topic: string} {
+        return {
+            title,
+            body: from,
+            topic: process.env.APN_BUNDLEID,
+        }
+    }
 
-	private getTransactionType(transaction: {operations: any[]}): string {
-		const operations = transaction.operations;
+    private getTransactionType(transaction: {operations: any[]}): string {
+        const operations = transaction.operations;
 
-		return operations.length >= 1 ? operations[0].type : "transfer";
-	}
+        return operations.length >= 1 ? operations[0].type : "transfer";
+    }
 
-	private send(token: string, data: any) {
-		return this.push.send(token, data).then((results: any) => {
-				return results;
-			}).catch((error: Error) => {
-				winston.info("Error sending notification: ", error);
-			});
-	}
+    private send(token: string, data: any) {
+        return this.push.send(token, data).then((results: any) => {
+                return results;
+            }).catch((error: Error) => {
+                winston.info("Error sending notification: ", error);
+            });
+    }
 }
