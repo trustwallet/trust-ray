95e13e44e42c3020c0a6973512c8fe0e2045e0e4
Mykola Radchenko <kolya182@gmail.com>
2018-01-23 13:05:28 -0800

    Move markets to file

---
+++ src/common/marketplace/marketsList.ts
@@ -0,0 +1,8 @@
+export const marketsList = [
+    {
+        "name": "Token Factory",
+        "url": "https://tokenfactory.netlify.com/",
+        "description": "Issue & Interact with Standard Token Contracts on Ethereum",
+        "image": "https://tokenfactory.netlify.com/images/icon.png"
+    }
+]
--- src/controllers/MarketplaceController.ts
+++ src/controllers/MarketplaceController.ts
@@ -1,18 +1,11 @@
 import { Request, Response } from "express";
 import { sendJSONresponse } from "../common/Utils";
+import { marketsList } from "../common/marketplace/marketsList";
 
 export class MarketplaceController {
-    private json = [
-        {
-            "name": "Token Factory",
-            "url": "https://tokenfactory.netlify.com/",
-            "description": "Issue & Interact with Standard Token Contracts on Ethereum",
-        }
-    ]
-
     getMarkets = (req: Request, res: Response) => {
         sendJSONresponse(res, 200, {
-            docs: this.json
+            docs: marketsList
         })
     }
 }
