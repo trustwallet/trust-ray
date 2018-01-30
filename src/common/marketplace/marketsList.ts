b5646069d642f297c1f2757eb8c86caeef3fb9ed
Mykola <kolya182@gmail.com>
2018-01-29 19:34:14 -0800

    Add more markets (#140)
    * And and sort markets by category
    
    * List of markets

--- src/common/marketplace/marketsList.ts
+++ src/common/marketplace/marketsList.ts
@@ -3,6 +3,36 @@ export const marketsList = [
         "name": "Token Factory",
         "url": "https://tokenfactory.netlify.com/",
         "description": "Issue & Interact with Standard Token Contracts on Ethereum",
-        "image": "https://tokenfactory.netlify.com/images/icon.png"
+        "image": "https://tokenfactory.netlify.com/images/icon.png",
+        "category": "social"
+    },
+    {
+        "name": "DNN",
+        "url": "https://dnn.media/",
+        "description": "A political news platform",
+        "image": "https://tokenfactory.netlify.com/images/icon.png",
+        "category": "social"
+    },
+    {
+        "name": "Cryptokitties",
+        "url": "https://www.cryptokitties.co/",
+        "description": "Collect and breed adorable digital cats",
+        "image": "https://www.cryptokitties.co/images/letterHead.png",
+        "category": "games"
+
+    },
+    {
+        "name": "CryptoPunks",
+        "url": "https://www.larvalabs.com/cryptopunks",
+        "description": "10,000 unique collectible punks",
+        "image": "https://www.larvalabs.com/cryptopunks/cryptopunk355.png",
+        "category": "games"
+    },
+    {
+        "name": "Name Bazaar",
+        "url": "https://namebazaar.io",
+        "description": "ENS name marketplace",
+        "image": "https://namebazaar.io/images/logo@2x.png",
+        "category": "marketplaces"
     }
 ]
