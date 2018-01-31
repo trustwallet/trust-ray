e95d46c2b2617e48b89f0f0037f0f0f35f0c0c64
Mykola Radchenko <kolya182@gmail.com>
2018-01-31 01:09:51 -0800

    Add all found dublicates on coinbase

--- src/common/tokens/contracts.ts
+++ src/common/tokens/contracts.ts
@@ -10,5 +10,44 @@ module.exports = {
     },
     "0x1234567461d3f8db7496581774bd869c83d51c93": {
         id: "bitclave"
+    },
+    "0x13f1b7fdfbe1fc66676d56483e21b1ecb40b58e2": {
+        id: "accelerator-network"
+    },
+    "0x8aa33a7899fcc8ea5fbe6a608a109c3893a1b8b2": {
+        id: "dao-casino"
+    },
+    "0xfae4ee59cdd86e3be9e8b90b53aa866327d7c090": {
+        id: "cpchain"
+    },
+    "0xe0c72452740414d861606a44ccd5ea7f96488278": {
+        id: "encryptotel-eth"
+    },
+    "0x2ae965cd3d2b6d186e87d9586fc3bdbfc667cacc": {
+        id: "global-jobcoin"
+    },
+    "0xb70835d7822ebb9426b56543e391846c107bd32c": {
+        id: "game"
+    },
+    "0x888666ca69e0f178ded6d75b5726cee99a87d698": {
+        id: "iconomi"
+    },
+    "0x647f274b3a7248d6cf51b35f08e7e7fd6edfb271": {
+        id: "maggie"
+    },
+    "0x449574c69f3a658794829ed81639a7a9ece041e1": {
+        id: "neo-gold"
+    },
+    "0xcfb98637bcae43c13323eaa1731ced2b716962fd": {
+        id: "nimiq"
+    },
+    "09800417b097c61b9fd26b3ddde4238304a110d5": {
+        id: "qbao"
+    },
+    "0x7dc4f41294697a7903c4027f6ac528c5d14cd7eb": {
+        id: "remicoin"
+    },
+    "0x6f6deb5db0c4994a8283a01d6cfeeb27fc3bbe9c": {
+        id: "smartbillions"
     }
 }
