538701adc5c3b9e96912c75a890a98e46c675d50
Michael Scoff <thetrustwallet@gmail.com>
2018-02-07 14:19:49 -0800

    Update list of dapps

--- src/common/marketplace/marketsList.ts
+++ src/common/marketplace/marketsList.ts
@@ -1,32 +1,52 @@
 export const marketsList = [
+    // {
+    //     "name": "Token Factory",
+    //     "url": "https://tokenfactory.netlify.com/",
+    //     "description": "Issue & Interact with Standard Token Contracts on Ethereum",
+    //     "image": "https://tokenfactory.netlify.com/images/icon.png",
+    //     "category": "social"
+    // },
+    // {
+    //     "name": "DNN",
+    //     "url": "https://dnn.media/",
+    //     "description": "A political news platform",
+    //     "image": "https://tokenfactory.netlify.com/images/icon.png",
+    //     "category": "social"
+    // },
     {
-        "name": "Token Factory",
-        "url": "https://tokenfactory.netlify.com/",
-        "description": "Issue & Interact with Standard Token Contracts on Ethereum",
-        "image": "https://tokenfactory.netlify.com/images/icon.png",
+        "name": "Cent",
+        "url": "https://beta.cent.co/",
+        "description": "Give wisdom, get money. Ask a question and offer and bounty for the best answers.",
+        "image": "https://beta.cent.co/img/logo_crop.png",
         "category": "social"
     },
     {
-        "name": "DNN",
-        "url": "https://dnn.media/",
-        "description": "A political news platform",
-        "image": "https://tokenfactory.netlify.com/images/icon.png",
+        "name": "Leeroy",
+        "url": "https://leeroy.io",
+        "description": "Leeroy is a decentralized social network build on Ethereum.",
+        "image": "https://ipfs.infura.io/ipfs/Qmb2YsYgyp6joBGJ9KrRcqGhDtbLjDdWCq3LFgv2duYReg",
         "category": "social"
     },
+    // {
+    //     "name": "Cryptokitties",
+    //     "url": "https://www.cryptokitties.co/",
+    //     "description": "Collect and breed adorable digital cats",
+    //     "image": "https://www.cryptokitties.co/images/letterHead.png",
+    //     "category": "games"
+    // },
+    // {
+    //     "name": "CryptoPunks",
+    //     "url": "https://www.larvalabs.com/cryptopunks",
+    //     "description": "10,000 unique collectible punks",
+    //     "image": "https://www.larvalabs.com/cryptopunks/cryptopunk355.png",
+    //     "category": "games"
+    // },
     {
-        "name": "Cryptokitties",
-        "url": "https://www.cryptokitties.co/",
-        "description": "Collect and breed adorable digital cats",
-        "image": "https://www.cryptokitties.co/images/letterHead.png",
-        "category": "games"
-
-    },
-    {
-        "name": "CryptoPunks",
-        "url": "https://www.larvalabs.com/cryptopunks",
-        "description": "10,000 unique collectible punks",
-        "image": "https://www.larvalabs.com/cryptopunks/cryptopunk355.png",
-        "category": "games"
+        "name": "OpenSea",
+        "url": "https://opensea.io/",
+        "description": "Peer-to-peer marketplace for rare digital items",
+        "image": "https://opensea.io/static/images/opensea-icon.png",
+        "category": "marketplaces"
     },
     {
         "name": "Name Bazaar",
