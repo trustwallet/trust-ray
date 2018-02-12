4ea4793ce258598a5c9cec26d00c1fe5a864bdb5
Mykola <kolya182@gmail.com>
2018-02-11 22:36:44 -0800

    Better naming

--- src/common/Config.ts
+++ src/common/Config.ts
@@ -2,6 +2,6 @@ const Web3 = require("web3");
 const config = require("config");
 
 export class Config {
-        static network = config.get("RPC");
+        static network = config.get("RPC_SERVER");
         static web3 = new Web3(new Web3.providers.HttpProvider(Config.network));
 }
