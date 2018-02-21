c9e02abf77c7ec3103b104d6b0a98036b407bfbb
Mykola <kolya182@gmail.com>
2018-02-21 13:17:38 -0800

    Add simple test

--- test/BlockchainParser.test.ts
+++ test/BlockchainParser.test.ts
@@ -1,20 +1,58 @@
+import { expect } from "chai";
+import "mocha";
+import { BlockchainParser } from "../src/common/BlockchainParser";
 
+describe("Test BlockchainParser", () => {
+    describe("Test getBlocksRange()", () => {
+        let getBlocksRange;
 
-describe("BlockchainParser Tests", () => {
+        beforeEach(() => {
+            getBlocksRange = new BlockchainParser().getBlocksRange;
+        });
 
-    it("should parse a transaction from a block", () => {
-        // TODO
+        it("Generate array of positive numbers", () => {
+            const range = getBlocksRange(1, 3);
+            expect(range).to.be.an("array");
+            expect(range).to.be.include.ordered.members([1, 2, 3]);
+        });
+        it("Generate array of positive numbers", () => {
+            const range = getBlocksRange(5, 5);
+            expect(range).to.be.an("array");
+            expect(range).to.be.include.ordered.members([5]);
+        });
     });
 
-    it("should parse the erc20 contract from a transaction", () => {
-        // TODO
-    });
+    describe("Test getNumberBlocks()", () => {;
+        let blockchainParser;
 
-    it("should parse the operation from a transaction", () => {
-        // TODO
-    });
+        beforeEach(() => {
+            blockchainParser = new BlockchainParser();
+        });
 
-    it("should update token balance from a transaction operation", () => {
-        // TODO
+        it("Should return only one block if difference between lastBlock and startBlock > 10 when ascending", () => {
+            const concurrentBlocks = 1;
+            const rebalanceOffsets = [30];
+            const ascending = true;
+            const startBlock = 100;
+            const lastBlock = 112
+            const getBlocksToParse = blockchainParser.getBlocksToParse(startBlock, lastBlock, concurrentBlocks);
+            const range = blockchainParser.getNumberBlocks(startBlock, lastBlock, ascending, rebalanceOffsets);
+
+            expect(range).to.be.an("array");
+            expect(range).to.be.include.ordered.members([100])
+        });
+
+        it("Should return only two block if difference between lastBlock and startBlock < 10 when ascending", () => {
+            const concurrentBlocks = 1;
+            const rebalanceOffsets = [30];
+            const ascending = true;
+            const startBlock = 100;
+            const lastBlock = 105
+            const getBlocksToParse = blockchainParser.getBlocksToParse(startBlock, lastBlock, concurrentBlocks);
+            const range = blockchainParser.getNumberBlocks(startBlock, lastBlock, ascending, rebalanceOffsets);
+
+            expect(range).to.be.an("array");
+            expect(range).to.be.include.ordered.members([70, 100])
+        });
     });
-});
+})
