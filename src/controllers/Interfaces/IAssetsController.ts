14a0d9bdb70ea39e21dd0f27d6bc9b21bae47017
Mykola <kolya182@gmail.com>
2018-03-03 19:26:28 -0800

    Add intreface

--- src/controllers/Interfaces/IAssetsController.ts
+++ src/controllers/Interfaces/IAssetsController.ts
@@ -2,5 +2,11 @@ export interface IAsset {
     image_url: string,
     name: string,
     external_link: string,
-    description: string
+    description: string,
+    asset_contract: IAssetContract
+}
+
+interface IAssetContract {
+    address: string,
+    name: string
 }
