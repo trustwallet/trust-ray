14ad8d7ce8bbc4ec4e502302c7b3b975df4e7bf1
Mykola <kolya182@gmail.com>
2018-03-10 00:02:03 -0800

    Serve assets only for main network
    Temporary fix

--- src/controllers/AssestsController.ts
+++ src/controllers/AssestsController.ts
@@ -9,9 +9,17 @@ import * as config from "config";
 const svg2png = require("svg2png");
 
 export class AssetsController {
+    private networkID: string;
     private openSeaURL: string = "https://opensea-api.herokuapp.com/assets/?order_by=auction_created_date&order_direction=desc&owner=";
 
     getAssets = async (req: Request, res: Response) => {
+        this.networkID = await this.setNetworkID();
+
+        if (this.networkID !== "1") {
+            return sendJSONresponse(res, 200, {
+                docs: []
+            });
+        }
         const address: string = req.query.address;
         try {
             const assetsByAddress = await this.getAssetsByAddress(address);
@@ -139,5 +147,22 @@ export class AssetsController {
             winston.error(`Error converting SVG`, errr)
         })
     }
-}
 
+    private setNetworkID() {
+        const rpcURL: string = config.get("RPC_SERVER");
+
+        return axios({
+            method: "post",
+            url: rpcURL,
+            data: {
+                jsonrpc: "2.0",
+                method: "net_version",
+                params: [],
+                id: 67
+            },
+        }).then((res: any) => res.data.result)
+        .catch((error: Error) => {
+            winston.error(`Error getting network ID`, error);
+        });
+    }
+}
