a77fc734a79ad166edf38640e14ba80070e81b6e
Viktor Radchenko <yazexel@gmail.com>
2018-03-20 18:12:38 -0700

    Update TokensParser.ts

--- src/common/TokensParser.ts
+++ src/common/TokensParser.ts
@@ -10,7 +10,7 @@ export class TokensParser {
 
     start(): void {
         BlockchainState.getBlockState().then(([blockInChain, blockInDb]) => {
-            const lastBlock = 2673139 // temp solution until blockchain parsed from the begining. Use blockInDb.lastBlock instead
+            const lastBlock = 2837114 // temp solution until blockchain parsed from the begining. Use blockInDb.lastBlock instead
             if (blockInDb.lastTokensBlock < lastBlock) {
                 this.startParsingNextBlock(blockInDb.lastTokensBlock, lastBlock)
             }
