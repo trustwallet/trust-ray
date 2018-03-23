d3e70c679d47bb80edd4ec37bb73c610c81dbc4c
BorisButakov <35042417+BorisButakov@users.noreply.github.com>
2018-03-22 17:52:37 -0700

    Update Contract map
    Added Zil and BTO

--- src/common/tokens/contracts.ts
+++ src/common/tokens/contracts.ts
@@ -1258,6 +1258,12 @@ export const contracts = {
      },
      "0xe469c4473af82217b30cf17b10bcdb6c8c796e75": {
          id: "exrnchain"
+     },
+     "0x36905fc93280f52362a1cbab151f25dc46742fb5": {
+         id: "bottos"
+     },
+     "0x05f4a42e251f2d52b8ed15e9fedaacfcef1fad27": {
+         id: "zilliqa"
      }
 }
 
