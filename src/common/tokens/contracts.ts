08c60571117aca260210b59114b14f1f3141140b
BorisButakov <35042417+BorisButakov@users.noreply.github.com>
2018-03-25 10:46:13 -0700

    Update Contracts_2 (#248)
    * Update Contracts_2
    
    Added REN - Republic Protocol
    
    * Update contracts.ts

--- src/common/tokens/contracts.ts
+++ src/common/tokens/contracts.ts
@@ -1261,6 +1261,12 @@ export const contracts = {
      },
      "0x36905fc93280f52362a1cbab151f25dc46742fb5": {
          id: "bottos"
+     },
+     "0x408e41876cccdc0f92210600ef50372656052a38": {
+         id: "republic-protocol"
+     },
+     "0x998b3b82bc9dba173990be7afb772788b5acb8bd": {
+         id: "banca"
      }
 }
 
