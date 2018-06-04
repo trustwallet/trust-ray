28bb00f04ec54c3017a617a60a7a7a1281bac6bb
Mykola <kolya182@gmail.com>
2018-06-03 19:27:38 -0700

    Change interface

--- src/controllers/Interfaces/ITokenPriceController.ts
+++ src/controllers/Interfaces/ITokenPriceController.ts
@@ -4,19 +4,25 @@ export interface IToken {
 }
 
 export interface IPrice {
-    id: string,
-    name: string,
-    symbol: string,
-    rank: string,
-    price_usd: string,
-    price_btc: string,
-    "24h_volume_usd": string,
-    market_cap_usd: string,
-    available_supply: string,
-    total_supply: string,
+    circulating_supply: number
+    id: number,
+    last_updated: number,
     max_supply: null,
-    percent_change_1h: string,
-    percent_change_24h: string,
-    percent_change_7d: string,
-    last_updated: string
+    name: string,
+    quotes: {USD: Icurrency}
+    rank: string,
+    symbol: string,
+    total_supply: number,
+    website_slug: string
+}
+
+interface Icurrency {
+    USD: {
+        market_cap: number
+        percent_change_1h: number
+        percent_change_24h: number
+        percent_change_7d: number
+        price: number
+        volume_24h: number
+    }
 }
